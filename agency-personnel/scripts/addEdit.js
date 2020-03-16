952cf6856a73707b0620ee56c11240df9f901169
Patrick_B <pb5191@gmail.com>
2020-03-16 14:51:31 -0400

    modal opening with Add button

--- agency-personnel/scripts/addEdit.js
+++ agency-personnel/scripts/addEdit.js
@@ -1,11 +1,24 @@
 //* Adding a new record
 
-function addRecord(e, element) {
+function addRecord(e, bloc) {
   e.stopPropagation();
-  const blockName = $(element)
+  const blockName = $(bloc)
     .attr("data-blockId")
     .split("-");
   console.log("blockName :", blockName);
+  $("#modalBloc").modal("toggle");
+  $(".modal-body form").remove();
+  $(".modal-body").append("<form id='modal-form'>Form Here</form>");
+  let list;
+  // switch (blockName) {
+  //   case "History":
+  //     list=
+  //     break;
+
+  //   default:
+  //     console.log('blockName :', `${blockName} Not Found!`);
+  //     break;
+  // }
   return false;
 }
 
